073833793db862c851e5574314111c2f64274aab
oznu <dev@oz.nu>
2017-11-22 17:50:19 +1100

    try and gen fake gpg key in travis

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-set -x 
+set -x
+
+# this gpg key is never going to be validated so it does not need to be secure
+cat >foo <<EOF
+     Key-Type: RSA
+     Key-Length: 2048
+     Subkey-Type: RSA
+     Subkey-Length: 2048
+     Name-Real: @oznu
+     Name-Comment: not my real gpg key
+     Name-Email: dev@oz.nu
+     Expire-Date: 0
+     Passphrase: abc
+     %commit
+     %echo done
+EOF
+
+gpg --batch --gen-key foo
+cp -R /root/.gnupg /toolkit/build_env/ds.x64-6.0/root/.gnupg
+
+exit 0
 
 mkdir /toolkit
 cd /toolkit
 git clone https://github.com/SynologyOpenSource/pkgscripts-ng.git 
 
 ./pkgscripts-ng/EnvDeploy -q -v 6.0 -p x64
+
+
 
 
