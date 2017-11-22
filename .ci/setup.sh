8e01188eb86d8bac33eb6f8281dab3e79dfc7f91
oznu <dev@oz.nu>
2017-11-22 17:29:11 +1100

    updated ci

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -x 
+
 mkdir /toolkit
 cd /toolkit
 git clone https://github.com/SynologyOpenSource/pkgscripts-ng.git 
 
-./pkgscripts-ng/EnvDeploy -v 6.0 -p x64
+./pkgscripts-ng/EnvDeploy -q -v 6.0 -p x64
 
 
