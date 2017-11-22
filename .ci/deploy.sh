92b88925544366da85011d4dc36de2083f42eb09
oznu <dev@oz.nu>
2017-11-22 18:45:45 +1100

    corrected spk extraction

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-mkdir $TRAVIS_BUILD_DIR/github-release
-rm -rf /toolkit/result_spk/*_debug.spk
-cp -R /toolkit/result_spk/*.spk $TRAVIS_BUILD_DIR/github-release/
+PKG_VERSION=$(cat $TRAVIS_BUILD_DIR/INFO.sh | grep version | awk -F '"' '{ print $2 }')
 
+mkdir $TRAVIS_BUILD_DIR/github-release
+
+cp /toolkit/result_spk/HomebridgeDocker-${PKG_VERSION}/HomebridgeDocker-x86_64-${PKG_VERSION}.spk $TRAVIS_BUILD_DIR/github-release/
