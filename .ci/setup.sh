588d12f71cbdc5a798267cc506d6d5ccfbf41ab9
oznu <dev@oz.nu>
2017-11-22 18:06:55 +1100

    try move around gpg keys

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -30,9 +30,11 @@ EOF
 gpg --batch --gen-key foo
 
 # copy gpg keys into chroot env
+mkdir -p /toolkit/build_env/ds.x64-6.0/home/travis/
 cp -R /home/travis/.gnupg /toolkit/build_env/ds.x64-6.0/root/.gnupg
-chown -R root:root /toolkit/build_env/ds.x64-6.0/root/.gnupg
-chmod -R 600 /toolkit/build_env/ds.x64-6.0/root/.gnupg
+cp -R /home/travis/.gnupg /toolkit/build_env/ds.x64-6.0/home/travis/.gnupg
+chown -R root:root /toolkit/build_env/ds.x64-6.0/root/.gnupg /toolkit/build_env/ds.x64-6.0/home/travis/.gnupg
+chmod -R 600 /toolkit/build_env/ds.x64-6.0/root/.gnupg /toolkit/build_env/ds.x64-6.0/home/travis/.gnupg
 
 # move build files into source dir
 mkdir -p /toolkit/source
