e4a9595233665a64cf8f79fb2118fbbbfd105627
oznu <dev@oz.nu>
2017-11-22 17:34:00 +1100

    fixed sudo

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -3,7 +3,7 @@
 set -x 
 
 sudo mkdir /toolkit
-sudo cd /toolkit
+cd /toolkit
 sudo git clone https://github.com/SynologyOpenSource/pkgscripts-ng.git 
 
 sudo ./pkgscripts-ng/EnvDeploy -q -v 6.0 -p x64
