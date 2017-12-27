dedbcb5553638ba6fde7cdbee145ba85ded80fe0
oznu <dev@oz.nu>
2017-12-27 13:00:44 +1100

    added option to install homebridge-config-ui-x in setup wizard

--- INFO.sh
+++ INFO.sh
@@ -3,7 +3,7 @@
 source /pkgscripts/include/pkg_util.sh
 
 package="HomebridgeDocker"
-version="1.0.3"
+version="1.0.4"
 maintainer="oznu"
 displayname="Homebridge Docker"
 description="HomeKit support for the impatient using Docker on Synology DSM."
@@ -13,5 +13,7 @@ arch="$(pkg_get_unified_platform)"
 thirdparty="yes"
 os_min_ver="6.1.3-15152"
 install_dep_packages="Docker>=1.11.2"
+dsmuidir="ui"
+dsmappname="oznu.homebridge"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
