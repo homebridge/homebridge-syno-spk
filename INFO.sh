7ca7e107cb2e12be7f033e5b11cc3fa6a7a2fa38
oznu <dev@oz.nu>
2018-01-17 17:30:01 +1100

    do not allow silent installs (no wizard)

--- INFO.sh
+++ INFO.sh
@@ -3,7 +3,7 @@
 source /pkgscripts/include/pkg_util.sh
 
 package="HomebridgeDocker"
-version="1.0.4"
+version="1.0.5"
 maintainer="oznu"
 displayname="Homebridge Docker"
 description="HomeKit support for the impatient using Docker on Synology DSM."
@@ -15,5 +15,10 @@ os_min_ver="6.1.3-15152"
 install_dep_packages="Docker>=1.11.2"
 dsmuidir="ui"
 dsmappname="oznu.homebridge"
+silent_install="no"
+qinst="false"
+adminprotocol="http"
+adminurl=""
+adminport="8581"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
