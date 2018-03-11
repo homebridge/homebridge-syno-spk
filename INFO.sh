34617a9b46d653031843957ce19eaad98d219195
oznu <dev@oz.nu>
2018-03-11 22:51:44 +1100

    added option for debian linux

--- INFO.sh
+++ INFO.sh
@@ -3,7 +3,7 @@
 source /pkgscripts/include/pkg_util.sh
 
 package="HomebridgeDocker"
-version="1.0.6"
+version="1.0.7"
 maintainer="oznu"
 displayname="Homebridge Docker"
 description="HomeKit support for the impatient using Docker on Synology DSM."
