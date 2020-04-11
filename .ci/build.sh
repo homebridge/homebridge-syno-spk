66becd865add2ae68a3b62477349e5c2f4043bee
oznu <dev@oz.nu>
2020-04-11 16:20:08 +1000

    Use GitHub Actions (#37)

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -x
+set -e
 
 cd /toolkit
 
 # move build files into source dir
 mkdir -p /toolkit/source
-cp -R $TRAVIS_BUILD_DIR /toolkit/source/homebridge-syno-spk
+cp -R $GITHUB_WORKSPACE /toolkit/source/homebridge-syno-spk
 
 # build spk
-./pkgscripts-ng/PkgCreate.py -c homebridge-syno-spk
+./pkgscripts-ng/PkgCreate.py -c homebridge-syno-spk -S
