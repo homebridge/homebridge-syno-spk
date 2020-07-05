c9e8fe1a5c57bcea249dd027556e1a679ffccb19
oznu <dev@oz.nu>
2020-07-05 16:31:11 +1000

    noarch

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -7,4 +7,4 @@ PKG_VERSION=$(cat $GITHUB_WORKSPACE/INFO.sh | grep version | awk -F '"' '{ print
 
 mkdir $GITHUB_WORKSPACE/github-release
 
-cp /toolkit/result_spk/HomebridgeDocker-${PKG_VERSION}/HomebridgeDocker-x86_64-${PKG_VERSION}.spk $GITHUB_WORKSPACE/github-release/
+cp /toolkit/result_spk/HomebridgeDocker-${PKG_VERSION}/HomebridgeDocker-${PKG_VERSION}.spk $GITHUB_WORKSPACE/github-release/
