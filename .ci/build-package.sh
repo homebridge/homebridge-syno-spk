1afa5c924c88ccd39bb4cab6b758da33ff7d3fc2
oznu <dev@oz.nu>
2021-12-19 15:51:24 +1100

    test ci

--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -14,3 +14,6 @@ wget https://nodejs.org/dist/$NODE_LTS/node-$NODE_LTS-linux-x64.tar.gz -O node-l
 
 # build spk
 /toolkit/pkgscripts-ng/PkgCreate.py -p braswell -c homebridge-syno-spk
+
+# copy spk to github workspace
+cp /toolkit/build_env/ds.braswell-7.0/image/packages/homebridge-braswell-$SPK_PACKAGE_VERSION.spk $GITHUB_WORKSPACE/
