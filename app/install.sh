9ce8946bf57fc1ee6f9dc4bc5b1258c2ed388996
oznu <dev@oz.nu>
2022-01-24 19:50:24 +1100

    update ui

--- app/install.sh
+++ app/install.sh
@@ -5,4 +5,4 @@ SCRIPTPATH="$(dirname $(realpath $0))"
 source "$SCRIPTPATH/source.sh"
 
 # install homebridge / homebridge ui
-npm install -g --unsafe-perm homebridge@latest homebridge-config-ui-x@4.41.6-test.3
+npm install -g --unsafe-perm homebridge@latest homebridge-config-ui-x@latest
