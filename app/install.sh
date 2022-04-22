489e2c49d519d1997286e3b44d4567aedd24fa31
oznu <dev@oz.nu>
2022-04-22 16:22:56 +1000

    update test versions

--- app/install.sh
+++ app/install.sh
@@ -7,4 +7,4 @@ source "$SCRIPTPATH/source.sh"
 
 # install homebridge / homebridge ui
 cd $HB_SERVICE_STORAGE_PATH
-pnpm install --unsafe-perm homebridge@latest homebridge-config-ui-x@4.43.1-test.7
+pnpm install --unsafe-perm homebridge@v1.4.1-beta.1 homebridge-config-ui-x@4.43.1-test.11
