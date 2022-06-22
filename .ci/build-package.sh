5981e039555c096d0b5ea22cb4fc93bda08adb5c
Roman Tymoshyk <tymoshyk.roman@gmail.com>
2022-06-23 02:18:34 +0300

    Update build-package.sh

--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -11,8 +11,8 @@ cp -R $GITHUB_WORKSPACE /toolkit/source/homebridge-syno-spk
 cd /toolkit/source/homebridge-syno-spk
 
 # get meta about most recent LTS
-NODE_OFFICIAL_LTS="$(curl -s https://nodejs.org/dist/index.json | jq -r 'map(select(.lts))[0].version')"
-NODE_LTS="$(echo ${NODE_OFFICIAL_LTS} | jq -r \"select([.files[] == \\\"linux-${NODE_ARCH}\\\"] | any).version\")"
+NODE_OFFICIAL_LTS="$(curl -s https://nodejs.org/dist/index.json | jq -r 'map(select(.lts))[0]')"
+NODE_LTS=$(echo "${NODE_OFFICIAL_LTS}" | jq -r "select([.files[] == \"linux-${NODE_ARCH}\"] | any).version")
 # check if arch officially supported
 if [ ! -z $NODE_LTS ];
 then
@@ -20,8 +20,9 @@ then
 else
    # if not then fetch arch from unofficial release
    NODE_LTS=$(curl -s https://unofficial-builds.nodejs.org/download/release/index.json | jq -r "map(select(.lts and ([.files[] == \"linux-${NODE_ARCH}\"] | any)))[0].version")
-   wget https://unofficial-builds.nodejs.org/download/release/${$NODE_LTS}/node-${$NODE_LTS}-linux-${NODE_ARCH}.tar.gz -O node-linux-${NODE_ARCH}.tar.gz
+   wget https://unofficial-builds.nodejs.org/download/release/${NODE_LTS}/node-${NODE_LTS}-linux-${NODE_ARCH}.tar.gz -O node-linux-${NODE_ARCH}.tar.gz
 fi
+
 
 # build spk
 /toolkit/pkgscripts-ng/PkgCreate.py -p $SPK_PLATFORM -c homebridge-syno-spk
